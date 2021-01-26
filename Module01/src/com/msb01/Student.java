e21c56d50956f58665da468fd4d99306a2623fa0
cz <340025123@qq.com>
2021-01-26 17:27:41 +0800

    添加CZ文件夹项目文件

---
+++ Module01/src/com/msb01/Student.java
@@ -0,0 +1,12 @@
+package src.com.msb01;
+
+/**
+ * @Auther: 城际云科技开发有限公司
+ * @Date: 2021/1/26 - 下午5:26
+ * @Description: src.com.msb01
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
